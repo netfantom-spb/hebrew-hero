80a65b4460a1c8a6498a03eb89cf12fabdbd25db
Viktor Egorov <netfantom.spb@gmail.com>
2023-02-22 00:09:01 +0300

    Basic extension builded with Rollup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hebrew-hero
 "Hebrew Helper" is a Chrome extension designed to assist Russian-speaking users in navigating Hebrew-language websites.
+
+
+```
+npm i -D rollup rollup-plugin-chrome-extension rollup-plugin-zip rollup-plugin-cleanup-dir
+```
---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+import cleanupDir from 'rollup-plugin-cleanup-dir'
+import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension';
+//import typescript from '@rollup/plugin-typescript';
+
+const isProduction = process.env.NODE_ENV === 'production',
+ manifestFile = 'src/manifest.json';
+
+export default {
+    input: manifestFile,
+    output: {
+        dir: 'build',
+        format: 'es',
+        chunkFileNames: path.join('chunks', '[name]-[hash].js'),
+        sourcemap: !isProduction
+    },
+    plugins: [
+        cleanupDir(),
+        chromeExtension(),
+        // typescript(),
+        simpleReloader(),
+        // isProduction && zip({ dir: 'releases' })
+    ],
+}       
